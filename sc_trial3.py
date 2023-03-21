7cb8c8cee174b5151383bbde165cda5194ba88b4
Watanabe Tsubasa (渡邊 翼) <watanabe-tsuba@aeonpeople.biz>
2023-03-21 14:48:27 +0900

    upd

--- sc_trial3.py
+++ sc_trial3.py
@@ -1,14 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-URL = 'https://stores.itoyokado.co.jp/070'
 
-res = requests.get(URL)
-soup = BeautifulSoup(res.text, 'html.parser')
+URL = 'https://www.itoyokado.co.jp/store/result.html'
 
+res_base = requests.get(URL)
+soup_base = BeautifulSoup(res_base.content, 'html.parser')
+
+url_names = soup_base.find_all('a', {"class": "-arrow"})
+
+url_list = []
+for url in url_names:
+    # print(url)
+    url_list.append(url["href"])
+    
+print(url_list)
+
+res = requests.get(url_list[0])
+# print(res.text)
+soup = BeautifulSoup(res.content, 'html.parser')
 div:BeautifulSoup = soup.select_one('#products > div')
 division_names:BeautifulSoup = div.find_all('div', {"class": "Products-name"})
-img_division:BeautifulSoup = div.find_all('img', {"class": "Products-productIcon"})
 
-for name, img in zip(division_names, img_division):
-        print(f'{name.get_text()}: {img["alt"]}')
+columns = []
+for name in division_names:
+    columns.append(name.get_text())
+
+df_result = pd.DataFrame(columns=columns)
+
+for i, url in enumerate(url_list):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, 'html.parser')
+    
+    # title = soup.title.string.split('|')[0]
+    try:
+        title = soup.find("h1").text
+        # div:BeautifulSoup = soup.select_one('#products > div')
+        div:BeautifulSoup = soup.select_one('#products > div')
+        img_division:BeautifulSoup = div.find_all('img', {"class": "Products-productIcon"})
+
+        list = []
+        for img in img_division:
+            list.append(img["alt"])
+            
+        df_result.loc[title] = list
+        
+        print(title)
+        if i <= 4:
+            print(df_result)
+    except:
+        continue
+
+df_result.to_excel('./result.xlsx')
